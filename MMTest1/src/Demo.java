964700a6eca598cae5603c52045f929cbcd644c0
piyushy1 <piyushy1@gmail.com>
2018-07-10 18:18:59 +0100

    Added a class

---
+++ MMTest1/src/Demo.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author piyush
+ * @date 10 Jul 2018
+ * @time 18:18:32
+ * @Institute Lero- NUIG
+ 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test1");
+
+	}
+
+}
